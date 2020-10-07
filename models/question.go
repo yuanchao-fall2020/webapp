35321e68340f619272f4b974536e01ab7d829904
chaoyi <yuan.chao@northeastern.edu>
2020-10-06 22:48:24 -0400

    third assignment

---
+++ models/question.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Question struct {
+	ID string `json:"question_id"`
+	QuestionCreated time.Time `json:"created_timestamp"`
+	QuestionUpdated time.Time `json:"updated_timestamp"`
+	User User `gorm:"ForeignKey:Id;AssociationForeignKey:UserId" json:"-"`
+	UserID string `json:"user_id"`
+	QuestionText string `json:"question_text"`
+	CategoryArr []Category `json:"categories" sql:"-"`
+	AnswerArr []Answer `json:"answers" sql:"-"`
+}
+
+/*type Question struct {
+	QuestionID string `json:"question_id"`
+	QuestionCreated time.Time `json:"created_timestamp"`
+	QuestionUpdated time.Time `json:"updated_timestamp"`
+	UserID string `json:"user_id" gorm:"foreignKey:ID"`
+	QuestionText string `json:"question_text"`
+}*/
