1b99c37db96f2890dc9b76d6a95136b253343693
chaoyi <yuan.chao@northeastern.edu>
2020-11-12 22:25:11 -0500

    a7 10

--- logger/logger.go
+++ logger/logger.go
@@ -15,7 +15,7 @@ func init() {
 	// set location of log file
 	//var logpath = build.Default.GOPATH + "/src/logger/info.log"
 	//var logpath = build.Default.GOPATH + "/opt/logs/csye6225.log"
-	var logpath = "/opt/logs/csye6225.log"
+	var logpath = "/opt/csye6225.log"
 
 	flag.Parse()
 	var file, err1 = os.Create(logpath)
