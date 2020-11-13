f4219d90c9807cf58b19661acf58696eef136ca6
chaoyi <yuan.chao@northeastern.edu>
2020-11-12 21:43:04 -0500

    a7 6

---
+++ logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+var (
+	Log      *log.Logger
+)
+
+
+func init() {
+	// set location of log file
+	//var logpath = build.Default.GOPATH + "/src/logger/info.log"
+	var logpath = "/opt/logs/csye6225.log"
+
+	flag.Parse()
+	var file, err1 = os.Create(logpath)
+
+	if err1 != nil {
+		panic(err1)
+	}
+	Log = log.New(file, "", log.LstdFlags|log.Lshortfile)
+	Log.Println("LogFile : " + logpath)
+}
