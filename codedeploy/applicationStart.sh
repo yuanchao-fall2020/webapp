74a8ea0dadff32c861a6301abdcd3f8b64c9282e
chaoyi <yuan.chao@northeastern.edu>
2020-11-12 23:47:29 -0500

    a7 16

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cw-agent.json \
+    -s
+
 sudo /opt/gin_demo > /dev/null 2> /dev/null < /dev/null &
 ##/opt/gin_demo
